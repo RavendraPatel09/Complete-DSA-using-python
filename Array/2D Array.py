db12df4b78c1733cfa02665fe5085704bd272e4a
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-10 23:54:07 +0530

    2D array using python concept

---
+++ Array/2D Array.py
@@ -0,0 +1,27 @@
+A = [
+    [1, 2, 3, 4],
+    [2, 4, 6, 8],
+    [3, 6, 9, 12]
+]
+print("Matrix A:")
+for i in range(3):
+    for j in range(4):
+        print(A[i][j], end=" ")
+    print()
+B = []
+for i in range(3):
+    row = [0] * 4 
+    B.append(row)
+print("\nMatrix B:")
+for i in range(3):
+    for j in range(4):
+        print(B[i][j], end=" ")
+    print()
+C = []
+for i in range(3):
+    C.append([0] * 4)
+print("\nMatrix C:")
+for i in range(3):
+    for j in range(4):
+        print(C[i][j], end=" ")
+    print()
