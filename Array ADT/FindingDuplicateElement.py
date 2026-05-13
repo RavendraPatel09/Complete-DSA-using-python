aa18d97f65a4079cc313bde431384cb294d163dd
patelsamerth9 <patelsamerth9@gmail.com>
2026-05-13 22:52:55 +0530

    Add FindingDuplicateElement.py to identify and count duplicate elements in an array

---
+++ Array ADT/FindingDuplicateElement.py
@@ -0,0 +1,11 @@
+arr = [1, 2, 2, 4, 5, 6, 6, 7, 7, 8, 9]
+n = len(arr)
+i = 0
+while i < n - 1:
+    if arr[i] == arr[i + 1]:
+        j = i + 1
+        while j < n and arr[i] == arr[j]:
+            j += 1
+        print(arr[i], "is repeated", j - i, "times")
+        i = j - 1
+    i += 1
